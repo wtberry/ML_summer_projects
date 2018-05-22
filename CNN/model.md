74a4a66baaa668563399289c8c2d94da419e30a5
Wataru Takahashi <wtakahashi1@my.hpu.edu>
2018-05-21 18:32:30 -1000

    added few layers to the model and modified main.py training

--- CNN/model.md
+++ CNN/model.md
@@ -1,14 +1,23 @@
 ### Model Specs
 
 #### Model Structures
-- Conv layers: 2
+- Conv layers: 2/3
+	* conv1: size 1, 20
+	* conv2: size 20, 20
 - MaxPool layers: 2
+	* MaxPool: 2
 - Activation: ReLU
 - Dense layers: 1
 
 #### Training tools/parameters
 - Loss: CrossEntropyLoss
+- epoch: 3 loops
+- optimizer: Adagrad
 
 #### Result
-- 2 loops on Training set
-- Test acc: 74%
+- Test acc: 85% with filter_num = 30, kernel_size = 5
+- Test acc: 83% with filter_num = 20, kernel_size = 5
+- Test acc: 87% with filter_num = 50, kernel_size = 5
+- Test acc: 84% with filter_num = 20, kernel_size = 3
+- Test acc: 84% with filter_num = 20, kernel_size = 5, 3 C&MP layers
+- Test acc: 88% with filter_num = 50, 35, 20, kernel_size = 3, 3 C&MP layers, leaky_ReLU
--- CNN/model.py
+++ CNN/model.py
@@ -13,25 +13,44 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
+        # defining the params for the model
+        fil_num1 = 50 
+        fil_num = 20
         # Defining layers needed
-        self.conv1 = nn.Conv2d(1, 20, kernel_size=5) #(input_depth, # of fileters)
-        self.conv2 = nn.Conv2d(20, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, fil_num1, kernel_size=3) #(input_depth, # of fileters)
+        self.conv2 = nn.Conv2d(fil_num1, 35, kernel_size=3)
+        self.conv3 = nn.Conv2d(35, fil_num, kernel_size=3)
         self.mp = nn.MaxPool2d(2) # maxpool layer
-        self.dense = nn.Linear(320, 10) # number of class is 10
+        self.dense1 = nn.Linear(875, 200) # number of class is 10
+        self.dense2 = nn.Linear(200, 10) # number of class is 10
 
 
     # forward pass
     def forward(self, x):
         in_size = x.size(0)
+
+        # 3 convolutions and maxpools
         c1 = self.conv1(x)
-        mp1 = self.mp(c1)
-        relu1 = F.relu(mp1)
+        mp1 = self.mp(c1) # 100x20x12x12
+        #print('size after first mp: ', mp1.shape)
+        relu1 = F.leaky_relu(mp1)
 
         c2 = self.conv2(relu1)
-        mp2 = self.mp(c2)
-        relu2 = F.relu(mp2)
-        out = relu2.view(in_size, -1) # flatten the output for Dense layer
+        mp2 = self.mp(c2) # 100x20x4x4
+        #print('size after 2nd MP: ', mp2.shape)
+        relu2 = F.leaky_relu(mp2)
 
-        out = self.dense(out)
+        c3 = self.conv3(relu2)
+        mp3 = self.mp(c3)
+        relu3 = F.leaky_relu(mp3)
+
+        out = relu2.view(in_size, -1) # flatten the output for Dense layer
+        #print('shape of out: ', out.shape)
+
+        ## 2 dense layers with leaky ReLU
+        out = self.dense1(out)
+        out = F.leaky_relu(out)
+        out = self.dense2(out)
+        out = F.leaky_relu(out)
         return out
 
